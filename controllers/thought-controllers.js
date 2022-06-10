03aacc6e19902a2b31366fb1781b98a984f50171
Alex Glaubitz <e.pirazzi@gmail.com>
2022-06-10 18:29:25 -0400

    get all thoughts

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -2,6 +2,16 @@ const { Thought } = require('../models');
 
 const thoughtController = {
   // get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .select('-__v')
+      .sort('-createdAt')
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 
   // get a single by _id
 
