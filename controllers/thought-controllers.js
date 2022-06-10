2b58eb5d8c88f361021e8e89729712406ece1af1
Alex Glaubitz <e.pirazzi@gmail.com>
2022-06-10 18:32:13 -0400

    find single thought by id

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -11,9 +11,24 @@ const thoughtController = {
         console.log(err);
         res.status(400).json(err);
       });
-  }
+  },
 
   // get a single by _id
+  getThoughtById({ params }, res) {
+    Thought.findOneAndUpdate({ _id: params.id })
+      .select('-__v')
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(400).json({ message: 'No thought found with this id.' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
   // post new thought
 
