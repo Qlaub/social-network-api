5821c71a36771d1ade3ff0a69c8e443357aa30ab
Alex Glaubitz <e.pirazzi@gmail.com>
2022-06-10 18:56:05 -0400

    update thought by id

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -48,11 +48,29 @@ const thoughtController = {
         res.json(dbUserData);
       })
       .catch(err => res.json(err));
-  }
+  },
 
   // post create reaction to thought
 
   // put update thought by _id
+  updateThoughtById({ body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: body.id },
+      { reactionBody: body.text },
+      { new: true }
+    )
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(400).json({ message: 'No thought found with this id.' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
 
   // delete thought by _id
 
