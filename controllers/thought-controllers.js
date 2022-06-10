4eb4024b3f0c03dfb8d71b7652b55515b3b02c45
Alex Glaubitz <e.pirazzi@gmail.com>
2022-06-10 18:25:30 -0400

    thought controllers pseduo code

---
+++ controllers/thought-controllers.js
@@ -0,0 +1,19 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+
+  // get a single by _id
+
+  // post new thought
+
+  // post create reaction to thought
+
+  // put update thought by _id
+
+  // delete thought by _id
+
+  // delete reaction by reactionId
+};
+
+module.exports = thoughtController;
