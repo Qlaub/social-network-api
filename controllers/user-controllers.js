301184aa699f3951dc74d958e4731e5b9d63c610
Alex Glaubitz <e.pirazzi@gmail.com>
2022-06-10 18:04:29 -0400

    update user by id

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -46,9 +46,20 @@ const userController = {
     User.create(body)
       .then(dbUserData => res.json(dbUserData))
       .catch(err => res.status(400).json(err));
-  }
+  },
 
   // put update user by _id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user found with this id. '});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
   // delete user by _id
 };
